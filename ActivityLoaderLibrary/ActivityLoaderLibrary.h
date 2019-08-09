de8e87c1bc6e7a6f1eb99ad6c99e012457f83291
Fazeel Ahmed <fazeel.a@sunandsandsports.com>
2019-08-09 14:27:38 +0400

    Initial Commit

---
+++ ActivityLoaderLibrary/ActivityLoaderLibrary.h
@@ -0,0 +1,19 @@
+//
+//  ActivityLoaderLibrary.h
+//  ActivityLoaderLibrary
+//
+//  Created by Fazeel Ahmed on 8/9/19.
+//  Copyright © 2019 Fazeel Ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ActivityLoaderLibrary.
+FOUNDATION_EXPORT double ActivityLoaderLibraryVersionNumber;
+
+//! Project version string for ActivityLoaderLibrary.
+FOUNDATION_EXPORT const unsigned char ActivityLoaderLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActivityLoaderLibrary/PublicHeader.h>
+
+
